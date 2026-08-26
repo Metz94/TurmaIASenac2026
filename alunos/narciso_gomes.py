fda7c32e13ddb8b3e34013350f9ef86a6dccbcba
Narciso Gomes <narciso.4265@aluno.pr.senac.br>
2026-08-26 20:49:02 -0300

    feat: cria joguinho da mega sena

--- alunos/narciso_gomes.py
+++ alunos/narciso_gomes.py
@@ -1,1 +1,8 @@
-print("Hello world!")
+print("Bem vindo ao meu joguinho da mega")
+print("acerte os 06 numeros e ganhe um parabéns, uma boa né?")
+aposta = input("Qual a sua aposta? ")
+
+if aposta == "02 02 02 02 02 02":
+    print("Parabéns, gastou sua sorte aqui e ganhou um parabéns ao invés de ficar milionário com a Mega Sena.")
+else:
+    print("Não foi dessa vez, tente novamente quando quiser.")
